869b36d6523c581b6a6bcedcff57e1c0c33c88ea
Sergey Solomevich <sergeysolomevich@gmail.com>
2018-02-05 20:24:14 +0300

    commit 4 - added ViewPager

--- app/src/main/java/com/solomevich/android/homecriminalintent/CrimeListFragment.java
+++ app/src/main/java/com/solomevich/android/homecriminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.solomevich.android.homecriminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -34,11 +35,22 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+
+        if (mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
 
@@ -66,9 +78,9 @@ public class CrimeListFragment extends Fragment {
         }
         @Override
         public void onClick(View v) {
-            Toast.makeText(getActivity(),
-                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+            Intent intent = CrimePagerActivity.newIntent(getActivity(),
+                    mCrime.getId());
+            startActivity(intent);
         }
     }
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
