005529ccd1a55d3959e1dbfb9f85ef3d9796dcb8
Sergey Solomevich <sergeysolomevich@gmail.com>
2018-02-08 20:54:38 +0300

    commit 6 - added toolbar

--- app/src/main/java/com/solomevich/android/homecriminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/solomevich/android/homecriminalintent/SingleFragmentActivity.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by 15 on 02.02.2018.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
     protected abstract Fragment createFragment();
     @Override
     public void onCreate(Bundle savedInstanceState) {
